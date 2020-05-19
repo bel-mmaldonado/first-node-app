64ba4fee2a2b02c945c03aaf4ff9a7c8e52d582b
Belen Maldonado <beelu.maldonado@gmail.com>
2020-05-19 16:50:48 -0300

    commit on master

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const logger = require('./logger');
 let pathObj = path.parse('C:\\Users\\Equipo\\Desktop\\incluit');
 logger.log('path', pathObj);
 let dir = pathObj.dir;
-let filesArray = [];
-
 
 fs.readdir(dir, (err, files) =>{
     if(err){
@@ -15,27 +13,21 @@ fs.readdir(dir, (err, files) =>{
     else{
         console.log('Listado de archivos contenidos en la ruta: ', files);
         console.log('Listado ordenado descendente: ', files.reverse());
+        comienzaCon(files, 'M');
 
-        files.forEach(file => {
-            filesArray.push(files);
-        });
-        //comienzaCon(filesArray, "M");
+        };
 
+    });
 
-    } 
-});
-
-/*comienzaCon = (filesArray, letter) => {
+comienzaCon = (files, letter) => {
     var cont = 0;
-    for (var i=0; i<filesArray.length; i++){
-        if (filesArray[i].startsWith(letter)){
+    for (var i=0; i<files.length; i++){
+        if (files[i].startsWith(letter)){
             cont ++;
         }
     }
     console.log('Cantidad de archivos que comienzan con ' + letter + ': ', cont);
-
     }
-*/
 
 
 
